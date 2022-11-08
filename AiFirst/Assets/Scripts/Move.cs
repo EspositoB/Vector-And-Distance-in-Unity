ae20c1149840bb6f6e761249f5b85395e91a3d4f
inthemoodforSena <senacankahraman@gmail.com>
2022-11-08 15:05:36 +0300

    Vectors And Distance in Unity
    Moving NPC towards Character with calculating distance between Character and NPCs

---
+++ AiFirst/Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    [SerializeField] float moveSpeed = 2f;
+
+    void Start() {
+
+        //direction = goal.transform.position - transform.position;
+        
+        //this.transform.Translate(direction);
+    }
+
+    private void LateUpdate() 
+    {
+        direction = goal.transform.position - transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if(direction.magnitude > 2)
+        {
+            Vector3 velocity = direction.normalized*moveSpeed*Time.deltaTime;
+            this.transform.position += velocity;
+        }  
+    }
+}
